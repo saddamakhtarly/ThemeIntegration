c97772c69ff3bc2261572b6d7628184596fa9466
Saddam Akhtar <saddamakhtar.ly@gmail.com>
2020-10-23 00:32:47 +0500

    Add project files.

---
+++ Theme/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Theme.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult I401()
+        {
+            return View();
+        }
+
+
+        public ActionResult I404()
+        {
+            return View();
+        }
+
+        public ActionResult I500()
+        {
+            return View();
+        }
+        public ActionResult Charts()
+        {
+            return View();
+        }
+
+
+        public ActionResult LayoutSidenavLight()
+        {
+            return View();
+        }
+
+        public ActionResult layoutStatic()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Password()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        public ActionResult Tables()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
